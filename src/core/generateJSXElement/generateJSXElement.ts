52024653c2604c21975f3ebe19a0fb8be2557f31
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-07-22 00:50:44 +0200

    Feat: generateJSXElement

---
+++ src/core/generateJSXElement/generateJSXElement.ts
@@ -0,0 +1,35 @@
+import { types } from "@babel/core";
+import { jsxPragma } from "../../plugins/main.plugin";
+import { isFirstCharacterUppercase } from "../../utils/utils";
+import { generateAttributes, generateChildren } from "..";
+
+const { isJSXMemberExpression } = types;
+
+const generateJSXElement = (element: types.JSXElement): string => {
+    const { openingElement } = element;
+    const attributes = generateAttributes(openingElement.attributes);
+
+    console.log("=============================================================");
+    console.log("Main element:", element.openingElement);
+    console.log("=============================================================");
+    
+    
+    let transformedJSX = jsxPragma;
+    if (isJSXMemberExpression(openingElement.name)) {
+        const tagName = openingElement.name.property.name;
+        transformedJSX += `(${tagName}, ${attributes}`;
+    } else {
+        const tagName = openingElement.name.name.toString();
+        if (isFirstCharacterUppercase(tagName))
+            return `Button(${attributes})`
+
+        transformedJSX += `("${tagName}", ${attributes}`;
+    }
+
+    const children = generateChildren(element.children);
+    transformedJSX += children ? `, ${children})` : ')';
+
+    return transformedJSX;
+}
+
+export default generateJSXElement;
---
+++ src/core/generateJSXElement/index.ts
@@ -0,0 +1,1 @@
+export { default as generateJSXElement } from './generateJSXElement';
