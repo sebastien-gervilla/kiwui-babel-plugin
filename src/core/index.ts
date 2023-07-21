18f19eec505c6d80c556d24f53d59e110942102d
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-07-22 00:51:10 +0200

    Fix: Imports

---
+++ src/core/index.ts
@@ -0,0 +1,7 @@
+export { generateJSXElement } from './generateJSXElement';
+export { generateAttributes } from './generateAttributes';
+export { generateChildren } from './generateChildren';
+export { generateExpression } from './generateExpression';
+export { generateJSXFragment } from './generateJSXFragment';
+export { generateAttributeValue } from './generateAttributeValue';
+export { generateSpreadAttribute } from './generateSpreadAttribute';
