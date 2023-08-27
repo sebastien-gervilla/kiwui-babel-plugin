d44cc4af0596a298558b3440932505f979754f03
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-08-27 14:47:29 +0200

    Fix: Add imports

--- src/core/generateExpression/generateStatement/index.ts
+++ src/core/generateExpression/generateStatement/index.ts
@@ -3,4 +3,7 @@ export { generateDoWhileStatement } from './generateDoWhileStatement';
 export { generateIfStatement } from './generateIfStatement';
 export { generateWhileStatement } from './generateWhileStatement';
 export { generateForStatement } from './generateForStatement';
-export { generateForOfStatement } from './generateForOfStatement'
+export { generateForOfStatement } from './generateForOfStatement';
+export { generateTryStatement , generateCatchClause , generateFinallyBlock } from './generateTryCatchStatement';
+export { generateThrowStatement } from './generateThrowStatement';
+export { generateForInStatement } from './generateForInStatement'
