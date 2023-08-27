85f136dcfcd16fbd17943e2d766a10c20ef0dbbb
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-08-27 14:10:14 +0200

    Fix: double operators in AssignementExpression

--- src/core/generateExpression/generateAssignementExpression/generateAssignementExpression.ts
+++ src/core/generateExpression/generateAssignementExpression/generateAssignementExpression.ts
@@ -13,8 +13,11 @@ const {
 
 
 export const generateAssignementExpression = (expression : types.AssignmentExpression ) => {
+    
     let left = '';
     left = generateExpression(expression.left);
     const right = generateExpression(expression.right);
-    return `${left} = ${right}`;
+    
+    return `${left} ${expression.operator} ${right}`;
+
 }
