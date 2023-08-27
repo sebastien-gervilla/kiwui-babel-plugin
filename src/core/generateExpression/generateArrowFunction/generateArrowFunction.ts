87571e0a94268be0fb50e2b4f72a2376e05a182e
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-08-27 14:45:25 +0200

    Feat: Async Arrow Function

--- src/core/generateExpression/generateArrowFunction/generateArrowFunction.ts
+++ src/core/generateExpression/generateArrowFunction/generateArrowFunction.ts
@@ -11,7 +11,7 @@ const {
     isArrayPattern
 } = types
 
-export const generateArrowFunction = (expression : types.ArrowFunctionExpression) => {
+export const generateArrowFunction = (expression: types.ArrowFunctionExpression): string => {
     const params = expression.params.map(param => {
         if (isIdentifier(param)) {
             return param.name;
@@ -24,14 +24,16 @@ export const generateArrowFunction = (expression : types.ArrowFunctionExpression
         }
     }).filter(Boolean).join(', ');
 
+    const isAsync = expression.async ? "async " : ""; // Ajoutez "async" si la fonction est asynchrone
+
     const body = isBlockStatement(expression.body)
         ? generateBlockStatement(expression.body) 
         : expression.body
             ? generateExpression(expression.body)
             : '{ }'; 
 
-    return params ? `(${params}) => ${body}` : `() => ${body}`;
-}
+    return params ? `${isAsync}(${params}) => ${body}` : `${isAsync}() => ${body}`;
+};
 
 
 const generateBlockStatement = (block: types.BlockStatement, wrapWithBraces: boolean = true): string => {
