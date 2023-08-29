f04e88e706447f6e0a82038bfe5286dbd07f762a
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-08-29 23:41:22 +0200

    Imp: Better error handling

--- src/core/generateExpression/generateObjectPatternProperties/generateObjectPatternProperties.ts
+++ src/core/generateExpression/generateObjectPatternProperties/generateObjectPatternProperties.ts
@@ -13,35 +13,21 @@ const {
 export const generateObjectPatternProperties = (pattern: types.ObjectPattern): string => {
     return pattern.properties
         .map(prop => {
-            if (isSpreadElement(prop)) {
-                // if (isIdentifier(prop.argument)) {
-                //     return `...${prop.argument.name}`;
-                // } else if (isArrayExpression(prop.argument )) {
-                //     const elements = prop.argument.elements
-                //         .map(element => {
-                //             if (isIdentifier(element)) {
-                //                 return element.name;
-                //             }
-                //             return '';
-                //         })
-                //         .filter(Boolean)
-                //         .join(', ');
-                //     return `...[${elements}]`;
-                // }
-                return '';
-            } else if (isObjectProperty(prop)) {
+            if (isObjectProperty(prop)) {
                 if ((isIdentifier(prop.key) || isStringLiteral(prop.key)) &&
                     (isIdentifier(prop.value) || isExpression(prop.value))) {
                     const key = isIdentifier(prop.key) ? prop.key.name : prop.key.value;
                     const value = generateExpression(prop.value as types.Expression);
                     return `${key}: ${value}`;
                 }
-            } else if (isRestElement(prop)) {
-                if (isIdentifier(prop.argument)) {
-                    return `...${prop.argument.name}`;
-                }
+
                 return '';
+            } 
+            
+            if (isIdentifier(prop.argument)) {
+                return `...${prop.argument.name}`;
             }
+
             return '';
         })
         .filter(Boolean)
--- src/core/generateExpression/generatePattern/generatePattern.ts
+++ src/core/generateExpression/generatePattern/generatePattern.ts
@@ -5,18 +5,26 @@ import { types } from "@babel/core";
 const {
     isIdentifier,
     isObjectPattern,
-    isArrayPattern
+    isArrayPattern,
+    isPattern
     
 } = types
 
 export const generatePattern = (param: types.Identifier | types.Pattern | types.RestElement, init?: types.Expression): any => {
     if (isIdentifier(param)) {
         return param.name;
-    } else if (isObjectPattern(param)) {
-        return `{ ${generateObjectPatternProperties(param)} }`;
-    } else if (isArrayPattern(param)) {
-        return `[ ${generateArrayPatternElements(param)} ]`;
-    } else {
-        return '';
     }
+    
+    if (isObjectPattern(param)) {
+        return `{ ${generateObjectPatternProperties(param)} }`;
+    }
+    
+    if (isArrayPattern(param)) {
+        return `[ ${generateArrayPatternElements(param)} ]`;
+    }
+
+    if (isPattern(param))
+        throw new Error("Pattern not supported");
+    
+    return ''
 }
