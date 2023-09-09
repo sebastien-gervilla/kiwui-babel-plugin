54106e9832f599e85141782b0d873d32ed01be74
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-09-09 10:52:26 +0200

    Feat: generatePattern generator

---
+++ src/core/generate/generatePattern/generatePattern.ts
@@ -0,0 +1,14 @@
+// Types
+import { types } from "@babel/core";
+import { AliasMap } from "../generate.types";
+
+// Generation functions
+import { generateAssignmentPattern } from "./generateAssignmentPattern";
+import { generateArrayPattern } from "./generateArrayPattern";
+import { generateObjectPattern } from "./generateObjectPattern";
+
+export const patternGenerator: AliasMap<types.Pattern> = {
+    AssignmentPattern: generateAssignmentPattern,
+    ArrayPattern: generateArrayPattern,
+    ObjectPattern: generateObjectPattern
+}
