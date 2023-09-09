f65b0c63177da3b5e16484dee9c440ade9af3e56
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-09-09 10:49:58 +0200

    Clean: Moved and cleaned generateConditionalExpression

--- src/core/generateExpression/generateConditionalExpression/generateConditionalExpression.ts
+++
@@ -1,9 +0,0 @@
-import { types } from "@babel/core";
-import generateExpression from "../generateExpression";
-
-export const generateConditionalExpression = (expression : types.ConditionalExpression) => {
-    const test = generateExpression(expression.test);
-    const consequent = generateExpression(expression.consequent);
-    const alternate = generateExpression(expression.alternate);
-    return `${test} ? ${consequent} : ${alternate}`;
-}
