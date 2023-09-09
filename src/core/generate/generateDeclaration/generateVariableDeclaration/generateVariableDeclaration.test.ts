c47c50aa70c18e812246728a33ebe23be46a159d
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-09-09 10:48:24 +0200

    Mod: Reworked generateVariableDeclaration

--- src/core/generate/generateDeclaration/generateVariableDeclaration/generateVariableDeclaration.test.ts
+++ src/core/generate/generateDeclaration/generateVariableDeclaration/generateVariableDeclaration.test.ts
@@ -1,22 +1,21 @@
 import { types } from "@babel/core";
-import { generateVariableDeclaration } from "../generateVariableDeclaration";
+import { generateVariableDeclaration } from ".";
 
-describe("generateVariableDeclaration", () => {
-  it("generates variable declaration for object pattern", () => {
+const runFirstTest = () => it("generates variable declaration for object pattern", () => {
     const objectPattern = types.objectPattern([
       types.objectProperty(types.identifier("x"), types.identifier("x")),
-      types.objectProperty(types.identifier("y"), types.identifier("y")),
+      types.objectProperty(types.identifier("y"), types.identifier("z")),
     ]);
     const variableDeclarator = types.variableDeclarator(objectPattern, types.identifier("obj"));
     const variableDeclaration = types.variableDeclaration("let", [variableDeclarator]);
 
     const result = generateVariableDeclaration(variableDeclaration);
 
-    const expected = "let { x: x, y: y } = obj";
+    const expected = "let { x: x, y: z } = obj";
     expect(result).toBe(expected);
-  });
-
-  it("generates variable declaration for array pattern", () => {
+});
+ 
+const runSecondTest = () => it("generates variable declaration for array pattern", () => {
     const arrayPattern = types.arrayPattern([
       types.identifier("x"),
       types.identifier("y"),
@@ -28,9 +27,9 @@ describe("generateVariableDeclaration", () => {
 
     const expected = "const [x, y] = arr";
     expect(result).toBe(expected);
-  });
+});
 
-  it("generates variable declaration for identifier", () => {
+const runThirdTest = () => it("generates variable declaration for identifier", () => {
     const identifier = types.identifier("x");
     const variableDeclarator = types.variableDeclarator(identifier);
     const variableDeclaration = types.variableDeclaration("var", [variableDeclarator]);
@@ -39,9 +38,9 @@ describe("generateVariableDeclaration", () => {
 
     const expected = "var x";
     expect(result).toBe(expected);
-  });
+});
 
-  it("generates variable declaration with init expression", () => {
+const runFourthTest = () => it("generates variable declaration with init expression", () => {
     const identifier = types.identifier("x");
     const initExpression = types.numericLiteral(42);
     const variableDeclarator = types.variableDeclarator(identifier, initExpression);
@@ -51,13 +50,24 @@ describe("generateVariableDeclaration", () => {
 
     const expected = "var x = 42";
     expect(result).toBe(expected);
-  });
+});
 
-  it("generates empty string for unsupported declaration", () => {
+const runFifthTest = () => it("generates empty string for unsupported declaration", () => {
     const unsupportedDeclaration = types.variableDeclaration("const", []);
 
     const result = generateVariableDeclaration(unsupportedDeclaration);
 
-    expect(result).toBe("");
-  });
+    expect(result).toBe("const ");
+});
+ 
+describe("generateVariableDeclaration", () => {
+    runFirstTest();
+
+    runSecondTest();
+
+    runThirdTest();
+
+    runFourthTest();
+
+    runFifthTest();
 });
