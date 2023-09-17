d96b2f91bedd1908fc3b5582463592079ca8def1
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-09-17 23:05:49 +0200

    Feat: Transforming fragment

--- src/plugins/transform-jsx.plugin.ts
+++ src/plugins/transform-jsx.plugin.ts
@@ -2,27 +2,34 @@ import { types, NodePath } from '@babel/core';
 import { generate } from '@/core';
 import injectImport from '../utils/injectKiwuiImport';
 
-export default function transformJSX() {
+let injected = false;
+
+const transformJSX = () => {
     return {
         visitor: {
             Program: {
                 enter(path: NodePath<types.Program>) {
-                    let injected = false;
-
                     path.traverse({
-                        JSXElement(path: NodePath<types.JSXElement>) {
-                            // Inject Kiwui import
-                            if (!injected) injected = injectImport(path);
-                            
-                            // Convert JSX element into a call to the custom pragma
-                            const parentElement = generate(path.node);
-                            path.replaceWithSourceString(parentElement);
-                        },
+                        JSXElement: handleJSX,
+                        JSXFragment: handleJSX
                     });
+                    
+                    injected = false;
                 },
             },
         },
     };
 };
 
+const handleJSX = (path: NodePath<types.JSXElement | types.JSXFragment>) => {
+    // Inject Kiwui import
+    if (!injected) injected = injectImport(path);
+
+    // Convert JSX element into a call to the custom pragma
+    const parentElement = generate(path.node);
+    path.replaceWithSourceString(parentElement);
+}
+ 
 export const JSX_PRAGMA = 'Kiwui.createElement';
+
+export default transformJSX;
