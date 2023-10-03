ad186ea97d31481a97f6068e0e4ad4c44fb0f1a3
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-10-03 13:58:50 +0200

    Mod: Rework generateUnaryExpression

--- src/core/generate/generateExpression/generateUnaryExpression/generateUnaryExpression.test.ts
+++ src/core/generate/generateExpression/generateUnaryExpression/generateUnaryExpression.test.ts
@@ -1,15 +1,54 @@
+import { types } from "@babel/core";
+import { generateUnaryExpression } from "./generateUnaryExpression";
 
-import { types } from '@babel/core';
-import { generateUnaryExpression } from './generateUnaryExpression'; // Assurez-vous d'importer correctement la fonction
+const runFirstTest = () => it("should generate a simple unary expression with a negation operator", () => {
+    const unaryExpression = types.unaryExpression("-", types.numericLiteral(42));
 
-describe('generateUnaryExpression', () => {
-    it('should generate the correct code for a unary expression', () => {
-        const expression = types.unaryExpression(
-            '!', types.identifier('value')
-        );
-        
-        const result = generateUnaryExpression(expression);
-        expect(result).toBe('!value');
-    });
+    const result = generateUnaryExpression(unaryExpression);
 
+    expect(result).toBe("- 42");
+});
+
+const runSecondTest = () => it("should generate a unary expression with a logical NOT operator", () => {
+    const unaryExpression = types.unaryExpression("!", types.booleanLiteral(true));
+
+    const result = generateUnaryExpression(unaryExpression);
+
+    expect(result).toBe("! true");
+});
+
+const runThirdTest = () => it("should generate a unary expression with typeof operator", () => {
+    const unaryExpression = types.unaryExpression("typeof", types.identifier("variable"));
+
+    const result = generateUnaryExpression(unaryExpression);
+
+    expect(result).toBe("typeof variable");
+});
+
+const runFourthTest = () => it("should generate a unary expression with void operator", () => {
+    const unaryExpression = types.unaryExpression("void", types.stringLiteral("Hello"));
+
+    const result = generateUnaryExpression(unaryExpression);
+
+    expect(result).toBe("void \"Hello\"");
+});
+
+const runFifthTest = () => it("should generate a unary expression with delete operator", () => {
+    const unaryExpression = types.unaryExpression("delete", types.memberExpression(
+        types.identifier("object"),
+        types.stringLiteral("property"),
+        true
+    ));
+
+    const result = generateUnaryExpression(unaryExpression);
+
+    expect(result).toBe("delete object[\"property\"]");
+});
+
+describe("generateUnaryExpression", () => {
+    runFirstTest();
+    runSecondTest();
+    runThirdTest();
+    runFourthTest();
+    runFifthTest();
 });
