ca501438537b2c41f273849a5a78c084136580f3
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-10-03 13:57:40 +0200

    Imp: Added async to arrow functions

--- src/core/generate/generateExpression/generateArrowFunction/generateArrowFunction.ts
+++ src/core/generate/generateExpression/generateArrowFunction/generateArrowFunction.ts
@@ -4,10 +4,10 @@ import { generate, generateFromArray } from "@/core";
 export const generateArrowFunction = (expression: types.ArrowFunctionExpression): string => {
     const params = generateFromArray(expression.params);
 
-    const isAsync = expression.async ? "async " : "";
+    const prefixes = expression.async ? "async " : "";
     const body = generate(expression.body);
 
     return params 
-        ? `${isAsync}(${params}) => ${body}` 
-        : `${isAsync}() => ${body}`;
+        ? `${prefixes}(${params}) => ${body}` 
+        : `${prefixes}() => ${body}`;
 };
