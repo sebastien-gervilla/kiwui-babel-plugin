0d2c683ca369afa11ecb64640001a972ca31bfdd
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-10-04 12:56:51 +0200

    Imp: ObjectProperty computed, and tests

--- src/core/generate/generateProperty/generateObjectProperty/generateObjectProperty.ts
+++ src/core/generate/generateProperty/generateObjectProperty/generateObjectProperty.ts
@@ -4,5 +4,7 @@ import { generate } from "@/core";
 export const generateObjectProperty = (property: types.ObjectProperty) => {
     const key = generate(property.key);
     const value = generate(property.value);
-    return `${key}: ${value}`;
+    return property.computed 
+        ? `[${key}]: ${value}`
+        : `${key}: ${value}`;
 }
