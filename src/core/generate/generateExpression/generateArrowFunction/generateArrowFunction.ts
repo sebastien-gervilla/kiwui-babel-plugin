33d065d45ebdf1ecba0ed552fa2ca6621e504607
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-10-04 12:56:36 +0200

    Feat: ArrowFunctionExpression tests

--- src/core/generate/generateExpression/generateArrowFunction/generateArrowFunction.ts
+++ src/core/generate/generateExpression/generateArrowFunction/generateArrowFunction.ts
@@ -1,13 +1,11 @@
 import { types } from "@babel/core";
 import { generate, generateFromArray } from "@/core";
 
-export const generateArrowFunction = (expression: types.ArrowFunctionExpression): string => {
+export const generateArrowFunction = (expression: types.ArrowFunctionExpression) => {
     const params = generateFromArray(expression.params);
 
-    const prefixes = expression.async ? "async " : "";
+    const prefixes = expression.async ? 'async ' : '';
     const body = generate(expression.body);
 
-    return params 
-        ? `${prefixes}(${params}) => ${body}` 
-        : `${prefixes}() => ${body}`;
+    return `${prefixes}(${params}) => ${body}`;
 };
