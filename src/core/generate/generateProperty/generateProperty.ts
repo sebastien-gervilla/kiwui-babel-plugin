9982b6190d1301f5c2a675b4222a174b3fbe9b42
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-10-05 16:11:54 +0200

    Feat: generateClassProperty

--- src/core/generate/generateProperty/generateProperty.ts
+++ src/core/generate/generateProperty/generateProperty.ts
@@ -3,9 +3,11 @@ import { types } from "@babel/core";
 import { GeneratorMap } from "../generate.types";
 
 // Generation functions
-import { generate } from "@/core";
 import { generateObjectProperty } from "./generateObjectProperty";
+import { generateClassProperty } from "./generateClassProperty";
 
 export const propertyGenerator: GeneratorMap<types.Property> = {
-    ObjectProperty: generateObjectProperty
+    ObjectProperty: generateObjectProperty,
+    ClassProperty: generateClassProperty,
+    ClassPrivateProperty: generateClassProperty
 }
