5fe94365529e5fb3062ef582734ad5f162c2d4f5
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-10-05 16:12:01 +0200

    Feat: ClassBody

--- src/core/generate/generateStandardized/generateStandardized.ts
+++ src/core/generate/generateStandardized/generateStandardized.ts
@@ -3,10 +3,11 @@ import { types } from "@babel/core";
 import { GeneratorMap } from "../generate.types";
 
 // Generation functions
-import { generate } from "@/core";
+import { generate, generateFromArray } from "@/core";
 
 export const standardizedGenerator: GeneratorMap<types.Standardized> = {
     RestElement: ({ argument }) => `...${generate(argument)}`,
     SpreadElement: ({ argument }) => `...${generate(argument)}`,
     TemplateElement: ({ value }) => value.raw,
+    ClassBody: ({ body }) => generateFromArray(body, '\n')
 }
