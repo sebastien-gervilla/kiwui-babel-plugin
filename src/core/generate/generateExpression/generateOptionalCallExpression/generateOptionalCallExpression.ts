73ce0971ea6dd344316648d6d9ae681a937a8439
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-10-06 23:36:09 +0200

    Clean: Removed log

--- src/core/generate/generateExpression/generateOptionalCallExpression/generateOptionalCallExpression.ts
+++ src/core/generate/generateExpression/generateOptionalCallExpression/generateOptionalCallExpression.ts
@@ -2,8 +2,6 @@ import { types } from "@babel/core";
 import { generate, generateFromArray } from "@/core";
 
 export const generateOptionalCallExpression = (expression: types.OptionalCallExpression) => {
-    console.log(expression);
-    
     const callee = generate(expression.callee);
     const args = generateFromArray(expression.arguments, ', ');
 
