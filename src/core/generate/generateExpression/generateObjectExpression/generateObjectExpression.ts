81a8b9aee98389faf3025f756eda9e5cfc03c769
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-12-05 16:23:30 +0100

    Fix: Object expression parenthesized

--- src/core/generate/generateExpression/generateObjectExpression/generateObjectExpression.ts
+++ src/core/generate/generateExpression/generateObjectExpression/generateObjectExpression.ts
@@ -1,5 +1,10 @@
 import { types } from "@babel/core";
 import { generateFromArray } from "@/core";
+import { isParenthesized } from "@/utils/is";
 
-export const generateObjectExpression = (expression: types.ObjectExpression) =>
-    `{ ${generateFromArray(expression.properties)} }`;
+export const generateObjectExpression = (expression: types.ObjectExpression) => {
+    const properties = generateFromArray(expression.properties);
+    return isParenthesized(expression)
+        ? `({ ${properties} })`
+        : `{ ${properties} }`;
+};
